63e51b07d4c8e1a48208431740c01a31b3a63c88
Jake Zuliani <jkzuliani40@gmail.com>
2024-09-25 15:37:43 -0400

    bugfix

--- src/node_service/endpoints.py
+++ src/node_service/endpoints.py
@@ -122,7 +122,7 @@ def execute(
         msg += "To fix this you can either:\n"
         msg += f" - update the cluster to run containers with python v{user_python_version}\n"
         msg += f" - update your local python version to be one of {cluster_python_versions}"
-        raise Response(msg, status_code=409)
+        return Response(msg, status_code=409)
 
     # call workers concurrently
     async def assign_worker(session, url):
