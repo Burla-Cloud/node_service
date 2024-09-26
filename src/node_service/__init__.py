728a3428a9aebefa07fdb39f0fcafe1a345377ca
Jake Zuliani <jkzuliani40@gmail.com>
2024-09-26 11:31:18 -0400

    test

--- src/node_service/__init__.py
+++ src/node_service/__init__.py
@@ -197,8 +197,15 @@ async def log_and_time_requests__log_errors(request: Request, call_next):
         traceback_str = format_traceback(tb_details)
         add_background_task(logger.log, str(e), "ERROR", traceback=traceback_str)
 
-    if response.status_code != 200:
+    if response.status_code != 200 and hasattr(response, "body"):
         response_text = await response.body().decode("utf-8", errors="ignore")
+        logger.log(f"non-200 status response: {response.status_code}: {response_text}", "WARNING")
+    elif response.status_code != 200 and hasattr(response, "body_iterator"):
+        body = b""
+        async for chunk in response.body_iterator:
+            body += chunk
+
+        response_text = body.decode("utf-8", errors="ignore")
         logger.log(f"non-200 status response: {response.status_code}: {response_text}", "WARNING")
 
     response_contains_background_tasks = getattr(response, "background") is not None
